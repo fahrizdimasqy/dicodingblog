556071325f7631d2b95caf4740caebe05fa40267
Fahriz Dimasqy <1197050037@student.uinsgd.ac.id>
2022-03-06 20:32:49 +0700

    fix off canvas menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './styles/main.css';
+import "./styles/main.css";
+const hamburgerButtonElement = document.querySelector("#hamburger");
+const drawerElement = document.querySelector("#drawer");
+const mainElement = document.querySelector("main");
+
+hamburgerButtonElement.addEventListener("click", (event) => {
+  drawerElement.classList.toggle("open");
+  event.stopPropagation();
+});
+
+mainElement.addEventListener("click", (event) => {
+  drawerElement.classList.remove("open");
+  event.stopPropagation();
+});
